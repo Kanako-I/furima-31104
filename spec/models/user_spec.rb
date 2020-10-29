60bbd8f5b787335f219b40ceb57482b0d366e895
Kanako-I <kanako.8ki@gmail.com>
2020-10-29 20:15:49 +0900

    ログイン、ログアウト機能実装

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,49 @@
+require 'rails_helper'
+# RSpec.describe User, type: :model do
+#   describe 'ユーザー新規登録' do
+#     it "nicknameが空だと登録できない" do
+#       @user.nickname = nil
+#       @user.valid?
+#       expect(@user.errors.full_messages).to include("Name can't be blank")
+#     end
+#     it "emailが空では登録できない" do
+#       @user.email = nil
+#       @user.valid?
+#       expect(@user.errors.full_messages).to include("Email can't be blank")
+#     end
+#     it "passwordが空だと登録できない" do
+#       @user.password = nil
+#       @user.valid?
+#       expect(@user.errors.full_messages).to include("Password can't be blank")
+#     end
+#     it "last_name_kanjiが空では登録できない" do
+#       @user.last_name_kanji = nil
+#       @user.valid?
+#       expect(@user.errors.full_messages).to include("last_name_kanji can't be blank")
+#     end
+#     it "first_name_kanjiが空では登録できない" do
+#       @user.first_name_kanji = nil
+#       @user.valid?
+#       expect(@user.errors.full_messages).to include("first_name_kanji can't be blank")
+#     end
+#     it "last_name_katakanaが空では登録できない" do
+#       @user.last_name_katakana = nil
+#       @user.valid?
+#       expect(@user.errors.full_messages).to include("last_name_katakana can't be blank")
+#     end
+#     it "first_name_katakanaが空では登録できない" do
+#       @user.first_name_katakana = nil
+#       @user.valid?
+#       expect(@user.errors.full_messages).to include("first_name_katakana can't be blank")
+#     end
+#     it "date_of_birthが空では登録できない" do
+#       @user.date_of_birth = nil
+#       @user.valid?
+#       expect(@user.errors.full_messages).to include("date_of_birth can't be blank")
+#     end
+#   end
+# end
+
+# RSpec.describe User, type: :model do
+#   pending "add some examples to (or delete) #{__FILE__}"
+# end
