057a3a0c7bc8266da3ae7166cbf51f027f078ac7
Kanako-I <kanako.8ki@gmail.com>
2020-11-02 14:27:52 +0900

    Activehash

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,4 +6,11 @@ class Item < ApplicationRecord
 
   has_one_attached :image
   belongs_to :user
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+
+  validates :title, :description, :user, :price, :category_id, :item_condition_id, :shipping_fee_id, :shipped_prefecture_id, :shipping_date_id, presence: true
+
+  validates :category_id, :item_condition_id, :shipping_fee_id, :shipped_prefecture_id, :shipping_date_id, numericality: { other_than: 1 }
 end
