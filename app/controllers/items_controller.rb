d234ba91c14a6ae62ad06208bb19c2f0643b37ad
Kanako-I <kanako.8ki@gmail.com>
2020-11-06 15:52:10 +0900

    レビュー後修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,6 @@
 class ItemsController < ApplicationController
+  before_action :set_item, only: [:show, :edit, :update]
+
   def index
     @items = Item.includes(:user).order('created_at DESC')
   end
@@ -12,29 +14,27 @@ class ItemsController < ApplicationController
     if @item.save
       redirect_to root_path
     else
-      render :new 
+      render :new
     end
   end
 
   def show
-    @item = Item.find(params[:id])
+    # @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
-    unless user_signed_in? && @item.user_id == current_user.id
-      redirect_to new_user_session_path
-    end
+    # @item = Item.find(params[:id])
+    redirect_to root_path unless user_signed_in? && @item.user_id == current_user.id
   end
 
   def update
-    @item = Item.find(params[:id])
+    # @item = Item.find(params[:id])
     if @item.update(message_params)
       redirect_to root_path
     else
       render :edit
     end
-  end 
+  end
 
   private
 
@@ -42,4 +42,7 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:title, :description, :user, :price, :category_id, :item_condition_id, :shipping_fee_id, :shipped_prefecture_id, :shipping_date_id, :image).merge(user_id: current_user.id)
   end
 
+  def set_item
+    @item = Item.find(params[:id])
+  end
 end
