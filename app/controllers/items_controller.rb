3e2a435a98a6212cc78db9afec9dd8f24a4ca66b
Kanako-I <kanako.8ki@gmail.com>
2020-11-06 15:18:12 +0900

    編集機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -12,7 +12,7 @@ class ItemsController < ApplicationController
     if @item.save
       redirect_to root_path
     else
-      render :new
+      render :new 
     end
   end
 
@@ -20,9 +20,26 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+    unless user_signed_in? && @item.user_id == current_user.id
+      redirect_to new_user_session_path
+    end
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(message_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end 
+
   private
 
   def message_params
     params.require(:item).permit(:title, :description, :user, :price, :category_id, :item_condition_id, :shipping_fee_id, :shipped_prefecture_id, :shipping_date_id, :image).merge(user_id: current_user.id)
   end
+
 end
